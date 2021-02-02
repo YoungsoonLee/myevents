fd032a619b649e3ede48f92d892f0019228b23e4
Ted Youngsoon Lee <youngtip@gmail.com>
2021-02-02 14:32:22 -0800

    .

--- src/lib/msgqueue/amqp/listener.go
+++ src/lib/msgqueue/amqp/listener.go
@@ -1,6 +1,9 @@
 package amqp
 
-import "github.com/streadway/amqp"
+import (
+	"github.com/YoungsoonLee/myevents/src/lib/msgqueue"
+	"github.com/streadway/amqp"
+)
 
 type amqpEventListener struct {
 	connection *amqp.Connection
@@ -18,4 +21,37 @@ func (a *amqpEventListener) setup() error {
 	_, err = channel.QueueDeclare(a.queue, true, false, false, false, nil)
 	return err
 
+}
+
+func NewAMQPEventListener(conn *amqp.Connection, queue string) (msgqueue.EventListener, error) {
+	listener := &amqpEventListener{
+		connection: conn,
+		queue:      queue,
+	}
+
+	err := listener.setup()
+	if err != nil {
+		return nil, err
+	}
+
+	return listener, nil
+}
+
+func (a *amqpEventListener) Listen(eventNames ...string) (<-chan masgqueue.Event, <-chan error, error) {
+	channel, err := a.connection.Channel()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer channel.Close()
+
+	for _, eventName := range eventNames {
+		if err := channel.QueueBind(a.queue, eventName, "events", false, nil); err != nil {
+			return nil, nil, err
+		}
+	}
+}
+
+func (a *ammqpEventListener) Listen(eventNames ...string (<-chan msgqueue.Event, <-chan error, error) {
+	
 }
